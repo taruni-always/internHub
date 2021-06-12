0a2c0ffa702ee8f20f31364992932916bfb0ef46
taruni-always <nayakantitaruni@gmail.com>
2021-06-12 16:51:41 +0530

    added edit profile

--- src/UI/Manager.java
+++ src/UI/Manager.java
@@ -69,6 +69,15 @@ public class Manager {
 		});
 		
 		editProfile = new JMenuItem("Edit profile");
+		editProfile.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				welcome.setText("");
+				frame.getContentPane().removeAll();
+				frame.repaint();
+				frame.add(back);
+				profileEdit(frame);
+			}
+		});
 		profileMenu.add(viewProfile);
 		profileMenu.add(editProfile);
 		
@@ -300,4 +309,189 @@ public class Manager {
 		frame.add(scroll);
 		
 	}
+	
+	public void profileEdit(JFrame frame) {
+		JButton update = new JButton("Update");
+		update.setBounds(200, 320, 154, 23);
+		update.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		update.setBackground(Color.WHITE);
+		
+		JLabel unameF, unameV, lnameF, fnameF, companyF, designationF, phoneF;
+		unameF = new JLabel("Username:");
+		unameV = new JLabel(manager_id);
+		fnameF = new JLabel("First name:");
+		lnameF = new JLabel("Last name:");
+		companyF = new JLabel("Company name");
+		designationF = new JLabel("Designation:");
+		phoneF = new JLabel("Phone number:");
+		
+		JTextField fnameV = new JTextField("");
+		JTextField lnameV = new JTextField("");
+		JTextField companyV = new JTextField(""); 
+		JTextField designationV = new JTextField("");
+		JTextField phoneV = new JTextField("");
+		
+		Connection con;
+		Statement s;
+		ResultSet rs;
+		try {
+			con = ConnectionManager.getConnection();
+			s = con.createStatement();
+			rs = s.executeQuery("select * from projectmanagers where manager_id = '" + manager_id + "'");
+			rs.next();
+			fnameV.setText(rs.getString(2));
+			lnameV.setText(rs.getString(3));
+			rs = s.executeQuery("select * from managerprofile where manager_id = '" + manager_id + "'");
+			rs.next();
+			companyV.setText(rs.getString(2));
+			designationV.setText(rs.getString(3));
+			phoneV.setText(rs.getString(4));
+			s.close();
+			con.close();
+		} 
+		catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		
+		unameF.setBounds(50, 30, 100, 30);
+		unameF.setFont(new Font("", Font.PLAIN, 20));
+		unameV.setBounds(250, 30, 100, 30);
+		unameV.setFont(new Font("", Font.PLAIN, 20));
+		
+		fnameF.setBounds(50, 70, 100, 30);
+		fnameF.setFont(new Font("", Font.PLAIN, 20));
+		fnameV.setBounds(250, 70, 100, 30);
+		fnameV.setFont(new Font("", Font.PLAIN, 20));
+		
+		lnameF.setBounds(50, 110, 100, 30);
+		lnameF.setFont(new Font("", Font.PLAIN, 20));
+		lnameV.setBounds(250, 110, 100, 30);
+		lnameV.setFont(new Font("", Font.PLAIN, 20));
+		
+		companyF.setBounds(50, 150, 150, 30);
+		companyF.setFont(new Font("", Font.PLAIN, 20));
+		companyV.setBounds(250, 150, 140, 30);
+		companyV.setFont(new Font("", Font.PLAIN, 20));
+		
+		phoneF.setBounds(50, 190, 150, 30);
+		phoneF.setFont(new Font("", Font.PLAIN, 20));
+		phoneV.setBounds(250, 190, 140, 30);
+		phoneV.setFont(new Font("", Font.PLAIN, 20));
+		
+		designationF.setBounds(50, 230, 150, 30);
+		designationF.setFont(new Font("", Font.PLAIN, 20));
+		designationV.setBounds(250, 230, 180, 30);
+		designationV.setFont(new Font("", Font.PLAIN, 20));
+		
+		update.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					validateProfile(fnameV.getText(), lnameV.getText(), companyV.getText(), phoneV.getText(), designationV.getText());
+				}
+				catch (Exception e1 ) {
+					e1.printStackTrace();
+				}
+			}
+		});
+		
+		frame.add(unameF);
+		frame.add(unameV);
+		frame.add(lnameF);
+		frame.add(lnameV);
+		frame.add(fnameF);
+		frame.add(fnameV);
+		frame.add(companyF);
+		frame.add(companyV);
+		frame.add(phoneF);
+		frame.add(phoneV);
+		frame.add(designationF);
+		frame.add(designationV);
+		frame.add(update);
+	}
+	
+	public void validateProfile(String fname, String lname, String company, String phone, String designation) {
+		String message = "";
+		if (fname.length() == 0) {
+			message = message + "First name cannot be empty!\n";
+		}
+		else {
+			for(char c : fname.toCharArray()) {
+				if (!Character.isAlphabetic(c)) {
+					message = message + "First name cannot have digits or special characters!\n";
+					break;
+				}
+			}
+		}
+		
+		for(char c : lname.toCharArray()) {
+			if (!Character.isAlphabetic(c)) {
+				message = message + "Last name cannot have digits or special characters!\n";
+				break;
+			}
+		}
+		
+		if (phone.length() == 0) {
+			message = message + "Phone number cannot be empty!\n";
+		}
+		else {
+			try {
+				Integer.parseInt(phone);
+				if (phone.length() != 10) {
+					message += "Phone number must have exactly 10 digits!\n";
+				}
+			}
+			catch (NumberFormatException e) {
+				message += "Phone number cannot have alphabets or special characters!\n";
+			}
+			catch (Exception e) {
+				message += "Phone number must have exactly 10 digits!\n";
+			}
+		}
+		
+		if (company.length() == 0) {
+			message = message + "Company name cannot be empty!\n";
+		}
+		else {
+			for(char c : company.replaceAll(" ", "").toCharArray()) {
+				if (!Character.isAlphabetic(c)  ) {
+					message = message + "Company name cannot have digits or special characters!\n";
+					break;
+				}
+			}
+		}
+		
+		if (designation.length() == 0) {
+			message = message + "College name cannot be empty!\n";
+		}
+		else {
+			for(char c : designation.replaceAll(" ", "").toCharArray()) {
+				if (!Character.isAlphabetic(c)  ) {
+					message = message + "Designation name cannot have digits or special characters!\n";
+					break;
+				}
+			}
+		}
+		
+		if (message.length() != 0) {
+			JOptionPane.showMessageDialog(new JFrame(), message, "error", JOptionPane.ERROR_MESSAGE);
+		}
+		else {
+			Connection con;
+			Statement s;
+			try {
+				con = ConnectionManager.getConnection();
+				s = con.createStatement();
+				s.executeQuery("update projectmanagers set firstname = '" + fname + "', lastname = '" + lname + "' where manager_id = '" + manager_id + "'");
+				s.executeQuery("update managerprofile set companyname = '" + company + "', designation = '" + designation + "', phonenumber = " + phone + " where manager_id = '" + manager_id + "'");
+				s.executeQuery("commit");
+				s.close();
+				con.close();
+			} 
+			catch (Exception e1) {
+				e1.printStackTrace();
+			}
+			JOptionPane.showMessageDialog(new JFrame(), "Updated successfully!");
+		}
+	}
+	
 }
